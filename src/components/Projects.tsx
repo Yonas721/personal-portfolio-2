e3cb7102fca94829ab90ea4f33da9a631a44ed9c
Yonas <jemberuyonas01@gmail.com>
2026-02-07 12:11:27 +0300

    projects page finished

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,28 +3,35 @@ import Finance from "../assets/projects/finance-A.jpg"
 import Medi from "../assets/projects/medi-1.jpg"
 const Projects = () => {
   return (
-    <div className="my-20">
+    <div className="my-20" id="projects">
       <div className="text-center mt-4">
         <h2 className="font-bold text-xl">Portfolio</h2>
         <h4 className="text-sm mt-2">Recent Projects</h4>
       </div>
 
-      <div className="flex justify-around p-3 mt-4">
-        <a href="" className=" text-gray-500 font-bold p-2 rounded-md">
+      <div className="flex gap-20 justify-center p-3 mt-4 ">
+        <a
+          href=""
+          className=" text-gray-500 font-bold p-2 px-6   border border-gray-300 rounded-md transition duration-1000 delay-150 hover:bg-gray-700 hover:text-white "
+        >
           All
         </a>
-        <a href="" className="text-gray-500 font-bold p-2 rounded-md">
+        <a
+          href=""
+          className="text-gray-500 font-bold p-2  border border-gray-300 rounded-md transition duration-1000 delay-150 hover:bg-gray-700 hover:text-white"
+        >
           Frontend
         </a>
-        <a href="" className="text-gray-500 font-bold p-2 rounded-md">
+        <a
+          href=""
+          className="text-gray-500 font-bold p-2  border border-gray-300 rounded-md transition duration-1000 delay-150 hover:bg-gray-700 hover:text-white"
+        >
           Backend
         </a>
       </div>
 
       <div className=" flex gap-26 justify-center py-4 ">
-       
         <div className="flex flex-col gap-10">
-          
           <div className="p-2">
             <Project img={Finance} title="Finance App" />
           </div>
