24b28280030a8ccde165558950dbaa5697ef2330
Yonas <jemberuyonas01@gmail.com>
2026-02-07 12:11:48 +0300

    nav bar linking

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,27 @@
 const NavBar = () => {
   return (
     <nav className="flex justify-around py-3  my-4 mx-2">
-      <h1 className="center font-medium px-2 py-1">Yonas</h1>
+     <a href="#home"> <h1 className="center font-medium px-2 py-1">Yonas</h1>
+     </a>
       <ul className="flex gap-4 px-2 text-sm  items-center">
-        <li><a href="#"> Home</a></li>
-        <li><a href="#"> About</a></li>
-        <li><a href="#"> Skills</a></li>
-        <li><a href="#"> Services</a></li>
-        <li><a href="#"> Portfolio</a></li>
-        <li><a href="#"> Contact</a></li>
+        <li>
+          <a href="#home"> Home</a>
+        </li>
+        <li>
+          <a href="#about"> About</a>
+        </li>
+        <li>
+          <a href="#skills"> Skills</a>
+        </li>
+        <li>
+          <a href="#services"> Services</a>
+        </li>
+        <li>
+          <a href="#projects"> Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact"> Contact</a>
+        </li>
       </ul>
     </nav>
   );
