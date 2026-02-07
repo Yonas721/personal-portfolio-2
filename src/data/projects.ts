dcd6fd57d21919db163a040c32a1558486127123
Yonas <jemberuyonas01@gmail.com>
2026-02-07 13:57:43 +0300

    created a file for list of projects

---
+++ src/data/projects.ts
@@ -0,0 +1,30 @@
+export const projects = [
+  {
+    id: "finance",
+    title: "Fiance Management System",
+    type: "back",
+    img: "../assets/projects/finance-A.jpg",
+    techs: ["Laravel", "postman", "git", "MySQL"],
+  },
+  {
+    id: "medi",
+    title: "MediConnect Integrated Healhtcare Solutions",
+    type: "back",
+    img: "../assets/projects/medi-1.jpg",
+    techs: ["Laravel", "postman", "git", "MySQL"],
+  },
+  {
+    id: "mak",
+    title: "Belhuman Hair",
+    type: "front",
+    img: "../assets/projects/finance-A.jpg",
+    techs: ["HTML", "Css", "TS", "React", "TailwindCss"],
+  },
+  {
+    id: "game",
+    title: "Video Game Site",
+    type: "front",
+    img: "../assets/projects/finance-A.jpg",
+    techs: ["HTML", "Css", "TS", "React", "TailwindCss"]
+  },
+];
