55c0f2d041669d400c59cd0d9d639b8c27c3f880
Yonas <jemberuyonas01@gmail.com>
2026-02-08 13:21:29 +0300

    alt property added on projects list

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,6 +7,8 @@ export const projects = [
     techs: ["Laravel", "postman", "git", "MySQL"],
     description:
       "As a lead developer, I architected and delivered a monolithic web application using Laravel, featuring robust RESTful APIs integrated with a MySQL database. I optimized complex queries and implemented secure data handling to ensure high performance and reliability. To meet core business needs, I engineered key integrations, including the Chapa payment gateway for scalable e-commerce transactions, and configured email services via Mailgun and SendGrid for automated user communications. Leading a team of three developers in an agile environment, I successfully coordinated sprints to deliver features on schedule and within budget.",
+
+    alt: "finance project image",
   },
   {
     id: 2,
@@ -16,6 +18,8 @@ export const projects = [
     techs: ["Laravel", "postman", "git", "MySQL"],
     description:
       "For MediConnect, I developed a secure and scalable healthcare management platform using Laravel and MySQL. The system centralized financial operations, implementing core features for patient billing, payment processing, and real-time transaction history tracking. Engineered to handle up to 1,000 daily transactions efficiently, the application was optimized for low latency and reliable performance within the integrated healthcare ecosystem.",
+
+    alt: "Mediconnect project image",
   },
   {
     id: 3,
@@ -25,6 +29,7 @@ export const projects = [
     techs: ["HTML", "Css", "TS", "React", "TailwindCss"],
     description:
       "Designed and developed a specialized e-commerce platform for a luxury human hair extensions brand. The website showcases an extensive, visually-rich catalog with detailed filtering by hair type, texture, length, and color. Built with a focus on conversion, it integrates secure payment processing, a streamlined checkout, and a mobile-first design to provide a seamless, trustworthy shopping experience for a discerning clientele.",
+    alt: "belhuman hair project image",
   },
   {
     id: 4,
@@ -34,5 +39,7 @@ export const projects = [
     techs: ["HTML", "Css", "TS", "React", "TailwindCss"],
     description:
       "Architected a dynamic community website for video game enthusiasts. The platform features a comprehensive game library with data from external APIs, user-generated reviews, and personalized collection tracking. Developed with performance in mind, it offers an engaging, interactive hub with a dark-themed UI, social features, and real-time updates to foster a dedicated gaming community.",
+
+    alt: "video game website image",
   },
 ];
