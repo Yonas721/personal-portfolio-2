d5e490901277c0d38ef9d464c4ae9e4eb85bc828
Yonas <jemberuyonas01@gmail.com>
2026-02-09 13:36:39 +0300

    change the position of the  project title in products page

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -13,27 +13,29 @@ const ProjectDetails = () => {
   return (
     <div className="flex flex-col">
       <div className="flex gap-20  px-4 py-8 mx-6 my-8">
-        <img
-          src={selectedImage ? selectedImage : project.img}
-          alt=""
-          className="max-w-150 border border-gray-200 rounded-md"
-        />
+        <div className="flex flex-col">
+          <h3 className="font-bold text-gray-500 mb-3 mx-4 text-lg">{project.title}</h3>
+          <img
+            src={selectedImage ? selectedImage : project.img}
+            alt=""
+            className="max-w-150 border border-gray-200 rounded-md"
+          />
+        </div>
+
         <div>
-          <h3 className="font-bold text-gray-500">{project.title}</h3>
+          <h3 className="font-bold text-gray-500">My Role</h3>
           <p className="mt-3">{project.description}</p>
 
-        
-            <ul className="flex mt-5  justify-around py-2">
-              {project.techs.map((tech) => (
-                <li
-                  key={tech}
-                  className="border border-gray-200 rounded-md px-2 py-1 text-gray-500 text-sm "
-                >
-                  {tech}
-                </li>
-              ))}
-            </ul>
-       
+          <ul className="flex mt-5  justify-around py-2">
+            {project.techs.map((tech) => (
+              <li
+                key={tech}
+                className="border border-gray-200 rounded-md px-2 py-1 text-gray-500 text-sm "
+              >
+                {tech}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
 
